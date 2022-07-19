b34a5fa0f7368961b200a9f39d4c6e14e9ebdf9d
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-07-19 13:43:21 +0200

    Add files via upload

--- sketch_Interrupt_register.ino
+++ sketch_Interrupt_register.ino
@@ -1,5 +1,49 @@
 /* 
+///////////////////////////////////////////////////////////////////////////////////////////
+Info primary Block (of 4 bits):
+
+      Dec        Bin          Hex
+       0           0          0x0
+       1           1          0x1  
+       2          10          0x2
+       3          11          0x3
+       4         100          0x4
+       5         101          0x5
+       6         110          0x6
+       7         111          0x7
+       8        1000          0x8
+       9        1001          0x9
+      10        1010          0xA
+      11        1011          0xB
+      12        1100          0xC
+      13        1101          0xD
+      14        1110          0xE
+      15        1111          0xF
+
+  then you can conbine two blocks...
+    8 bits value  -->   0x3D ---> (11+1101) ---> 111101 
+
+    And so... with Hexadecimal you can handle 8bits value in a more human "freindly way"....
+    that why memory is often writed in hexadecimal rather than binary 0x3D vs B111101
+    special on 32 or 64bits register...
+    4 bits  -> 0xF
+    8 bits  -> 0xFF
+    16 bits -> 0xFFFF
+    32 bits -> 0xFFFFFFFF
+    64 bits -> 0xFFFFFFFFFFFFFFFF
+    no way to remind a radom 64bits number sequence in binary: 
+    B1111111111011111101011111110101101111111111111111111111111111111
+    crazy... but not for computers... and that's computing !
+    
+    By the way the human can create smarter machine than himself 
+    but does machines can create humans smarter than machine too ? 
+
+///////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+/* 
    Setting the 8bits register of the chip ATMEGA328P U (Arduino Uno)
+   for interrupt on INT0 (PD2).
 */
 
 //Output pin D
@@ -17,7 +61,7 @@ volatile uint8_t* _SREG = (volatile uint8_t*) 0x5F;  // Global interupt
 volatile uint8_t* _EICRA = (volatile uint8_t*) 0x69; // Raising or Falling edge or other
 volatile uint8_t* _EIMSK = (volatile uint8_t*) 0x3D; // int0 or int1
 
-volatile uint8_t flag = 0;
+volatile uint8_t flag = 0; // global variable C++ optimisation disabled (volatile).
 
 void setup() {
 
@@ -26,6 +70,8 @@ void setup() {
   // Set input button and pull-up mode.
   *_DDRD &= ~0x4; // set PD2 as an input.
   *_PORTD |= 0x4; // pullup mode (20k in series internal before switch).
+
+  *_DDRB |= 0x20; //set PB5 as an output.
 
   // Config interrupt.
   *_SREG |= 0x80; // enable globla interrupt always on by default (just for exemple).
@@ -46,11 +92,6 @@ void setup() {
   // Serial.println(*_EIMSK, HEX);
   // delay(500);
 
-  *_DDRB |= 0x20; //setp PB5 as an output.
-
-  /*
-     Some test Comments 2  
-   */
 }
 
 void loop() {
